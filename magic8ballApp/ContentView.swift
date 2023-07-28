56e12b030de6c597f173a080279611ea0fe9eccf
Corinna Chen <cchen9023@bths.edu>
2023-07-28 10:45:29 -0400

    Initial Commit

---
+++ magic8ballApp/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  magic8ball
+//
+//  Created by Scholar on 7/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var answer = ""
+    var allAnswers = ["it is certain", "it is decidedly so", "without a doubt", "yes definitely", "you may rely on it", "as I see it, yes", "most likely", "outlook good", "yes", "signs point to yes", "reply hazy, try again", "ask again later", "better not tell you now", "cannot predict now", "concentrate and ask again", "dont count on it", "my reply is no", "my sources say no", "outlook not so good", "very doubtful"]
+    var body: some View {
+        
+        
+        ZStack{
+            Color(red: 0.8, green: 0.87, blue: 0.98)
+                .ignoresSafeArea()
+           
+            VStack {
+                
+                Button {
+                    answer = allAnswers.randomElement() ?? "oopsies try again"
+                } label: {
+                    Image("magic8ball")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                }
+                
+                Text("magic 8 ball says: ")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Text("")
+                Text(answer)
+                    .font(.title)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.red)
+                
+                
+            }
+            .padding(.all)
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ magic8ballApp/magic8ballAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  magic8ballAppApp.swift
+//  magic8ballApp
+//
+//  Created by Scholar on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct magic8ballAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
